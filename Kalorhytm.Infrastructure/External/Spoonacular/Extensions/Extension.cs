e609ae148520676d549b81beab2e393bcb549091
Mikołaj Winkiel <winkielmikolaj03@gmail.com>
2025-08-17 17:52:46 +0200

    Connected SpoonacularApi and start of MyFridge site.

---
+++ Kalorhytm.Infrastructure/External/Spoonacular/Extensions/Extension.cs
@@ -0,0 +1,19 @@
+﻿using Kalorhytm.Infrastructure.USDAFood;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+
+namespace Kalorhytm.Infrastructure.External.Spoonacular.Extensions
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddSpoonacular(IServiceCollection services)
+        {
+            var baseUrl = "api.spoonacular.com";
+
+            services.AddRefitClient<IUSDAFoodClient>()
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri(baseUrl));
+
+            return services;
+        }
+    }
+}
