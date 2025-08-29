2278baac835e1529d2f4e965584eb4b74a833666
Mikołaj Winkiel <winkielmikolaj03@gmail.com>
2025-08-29 21:05:20 +0200

    Added FluentValidation Nuget and wrote validations for Adding Ingredients

--- Kalorhytm.Logic/UseCases/MyFridgeUseCases/AddIngredientUseCase.cs
+++ Kalorhytm.Logic/UseCases/MyFridgeUseCases/AddIngredientUseCase.cs
@@ -1,4 +1,5 @@
-﻿using Kalorhytm.Contracts.Models.MyFridge;
+﻿using FluentValidation;
+using Kalorhytm.Contracts.Models.MyFridge;
 using Kalorhytm.Domain.Entities.MyFridge;
 using Kalorhytm.Domain.Interfaces.IRepositories;
 using Kalorhytm.Logic.Interfaces.IMyFridgeUseCases;
@@ -8,15 +9,25 @@ namespace Kalorhytm.Logic.UseCases.MyFridgeUseCases
     public class AddIngredientUseCase : IAddIngredientUseCase
     {
         private readonly IMyFridgeRepository _myFridge;
+        private readonly IValidator<MyFridgeModel> _validator;
 
-        public AddIngredientUseCase(IMyFridgeRepository myFridge)
+        public AddIngredientUseCase(IMyFridgeRepository myFridge,  IValidator<MyFridgeModel> validator)
         {
             _myFridge = myFridge;
+            _validator = validator;
         }
         
         
         public async Task<MyFridgeModel> ExecuteAsync(MyFridgeModel model)
         {
+            var validation = await _validator.ValidateAsync(model);
+
+            if (!validation.IsValid)
+            {
+                var errors = string.Join(", ", validation.Errors.Select(x => x.ErrorMessage));
+                throw new ValidationException(errors);
+            }
+            
             var entity = new MyFridgeEntity
             {
                 Name = model.Name,
---
+++ Kalorhytm.Logic/Validation/MyFridgeModelValidato.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using FluentValidation.Validators;
+using Kalorhytm.Contracts.Models;
+using Kalorhytm.Contracts.Models.MyFridge;
+
+namespace Kalorhytm.Logic.Validation
+{
+    public class MyFridgeModelValidator :AbstractValidator<MyFridgeModel>
+    {
+        public  MyFridgeModelValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Nazwa produktu jest wymagana.")
+                .MinimumLength(2).WithMessage("Nazwa musi mieć co najmniej 2 znaki.")
+                .MaximumLength(20).WithMessage("Nazwa nie może przekraczać 20 znaków.");
+
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("UserId nie może być pusty.");
+        }
+    }
+}
