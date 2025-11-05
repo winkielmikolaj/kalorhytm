cf2b5a8a22c4ed4ecfdcc06383ab9bf7bf245477
Mikołaj Winkiel <winkielmikolaj03@gmail.com>
2025-11-05 18:02:24 +0100

    Add Use Cases(Favourite Recipe)

--- Kalorhytm.Logic/Interfaces/IFavouriteRecipesUseCases/IAddFavouriteRecipeUseCase.cs
+++ Kalorhytm.Logic/Interfaces/IFavouriteRecipesUseCases/IAddFavouriteRecipeUseCase.cs
@@ -1,7 +1,9 @@
+using Kalorhytm.Contracts.Models.FavouriteRecipes;
+
 namespace Kalorhytm.Logic.Interfaces.IFavouriteRecipesUseCases
 {
     public interface IAddFavouriteRecipeUseCase
     {
-        
+        Task<FavouriteRecipesModel> ExecuteAsync(FavouriteRecipesModel model);
     }
 }
